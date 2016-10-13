246b74cc5a0ef2fee2fbddc38216450caa6d0813
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2016-10-12 17:35:06 -0700

    Use the module names that Django expects for the jobs app.

--- regparser/web/settings/base.py
+++ regparser/web/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_rq',
     'regparser.web',
-    'regparser.web.jobs',
+    'regparser.web.jobs.apps.JobsConfig',
     'regparser.web.index',
     'rest_framework'
 ]
